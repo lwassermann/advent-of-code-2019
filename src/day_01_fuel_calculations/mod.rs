bbfec1b828d49d59e407ad485332bbeb6e8dc014
Lars Wassermann <lars@bitcrowd.net>
2019-12-11 00:31:02 +0100

    Read the input

--- src/day_01_fuel_calculations/mod.rs
+++ src/day_01_fuel_calculations/mod.rs
@@ -1,3 +1,11 @@
+use std::fs;
+
+fn read_input() -> String {
+  return fs::read_to_string("./src/day_01_fuel_calculations/input.txt")
+    .expect("Something went wrong reading the input file from the day-folder:");
+}
+
 pub fn calculate() {
-  println!("Hello, world!");
+  let contents = read_input();
+  println!("{}", contents);
 }
