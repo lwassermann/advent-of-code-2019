19224241fc4ef8289a12b70bfd4d3de310ee1423
Lars Wassermann <lars@bitcrowd.net>
2019-12-11 03:11:00 +0100

    Load the provided program

--- src/day_02_intcode/mod.rs
+++ src/day_02_intcode/mod.rs
@@ -1,18 +1,29 @@
-fn load_memory(into: &mut Vec<u8>) {
-  into.push(99);
+use std::fs;
+use std::iter::FromIterator;
+
+fn load_memory(into: &mut Vec<u64>) {
+  let input = fs::read_to_string("./src/day_02_intcode/input.txt")
+    .expect("Something went wrong reading the input file from the day-folder:");
+  let program = Vec::from_iter(
+    input
+      .trim()
+      .split(",")
+      .map(|cell| cell.parse::<u64>().unwrap()),
+  );
+  into.extend_from_slice(&program);
 }
 
-fn lookup(memory: &[u8], position: usize) -> u8 {
+fn lookup(memory: &[u64], position: usize) -> u64 {
   let lvalue = memory[position] as usize;
   memory[lvalue]
 }
 
-fn set(memory: &mut [u8], position: usize, value: u8) {
+fn set(memory: &mut [u64], position: usize, value: u64) {
   let lvalue = memory[position] as usize;
   memory[lvalue] = value;
 }
 
-fn interpret(memory: &mut [u8]) {
+fn interpret(memory: &mut [u64]) {
   let mut position: usize = 0;
   loop {
     let opcode = memory[position];
@@ -42,6 +53,9 @@ pub fn calculate() {
   let mut memory = Vec::new();
   load_memory(&mut memory);
 
+  memory[1] = 12;
+  memory[2] = 2;
+
   interpret(&mut memory);
   println!("Value at position 0: {}", memory[0])
 }
@@ -52,22 +66,29 @@ mod tests {
 
   #[test]
   fn test_exit() {
-    let mut memory: &mut [u8] = &mut [99];
+    let mut memory: &mut [u64] = &mut [99];
     interpret(&mut memory);
     assert_eq!(memory, [99]);
   }
 
   #[test]
   fn test_addition() {
-    let mut memory: &mut [u8] = &mut [1, 0, 0, 0, 99];
+    let mut memory: &mut [u64] = &mut [1, 0, 0, 0, 99];
     interpret(&mut memory);
     assert_eq!(memory, [2, 0, 0, 0, 99]);
   }
 
   #[test]
   fn test_multiplication() {
-    let mut memory: &mut [u8] = &mut [2, 3, 0, 3, 99];
+    let mut memory: &mut [u64] = &mut [2, 3, 0, 3, 99];
     interpret(&mut memory);
     assert_eq!(memory, [2, 3, 0, 6, 99]);
+  }
+
+  #[test]
+  fn test_combination() {
+    let mut memory: &mut [u64] = &mut [1, 1, 1, 4, 99, 5, 6, 0, 99];
+    interpret(&mut memory);
+    assert_eq!(memory, [30, 1, 1, 4, 2, 5, 6, 0, 99]);
   }
 }
