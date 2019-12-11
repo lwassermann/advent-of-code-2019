11762c0ab9ddd65955c1e186c730e409c30ee45b
Lars Wassermann <lars@bitcrowd.net>
2019-12-11 03:39:34 +0100

    Create folder

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // mod day_01_fuel_calculations;
-mod day_02_intcode;
+// mod day_02_intcode;
+mod day_03_crossed_wires;
 
 fn main() {
     // day_01_fuel_calculations::calculate()
-    day_02_intcode::calculate()
+    // day_02_intcode::calculate()
+    day_03_crossed_wires::calculate()
 }
