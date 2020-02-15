7b5aada30949c339814dba5e47cad4dba876f0a3
Lars Wassermann <lars@bitcrowd.net>
2020-02-15 13:55:06 +0100

    WIP: Parse orbits and fiddle with the types

--- src/day_06_orbit_map/mod.rs
+++ src/day_06_orbit_map/mod.rs
@@ -1,9 +1,50 @@
+use std::collections::HashMap;
 use std::fs;
 
-fn load_uom() -> Vec<i64> {
+struct SpaceObject {
+  name: String,
+  orbiting: Option<&SpaceObject>,
+  distance: u32,
+}
+
+fn parse_uom(input: String) -> HashMap<String, SpaceObject> {
+  let mut objects_in_space = HashMap::new();
+  objects_in_space.insert(
+    "COM".to_string(),
+    SpaceObject {
+      name: "COM".to_string(),
+      orbiting: None,
+      distance: 0,
+    },
+  );
+
+  input
+    .lines()
+    .map(|orbit| orbit.split(')').collect())
+    .for_each(|[center_identifier, orbiting]| {
+      let center = objects_in_space.get(center_identifier);
+      let distance = match center {
+        Some(object) => object.distance + 1,
+        None => 0,
+      };
+      objects_in_space.insert(
+        orbiting,
+        SpaceObject {
+          name: orbiting,
+          orbiting: center,
+          distance: distance,
+        },
+      );
+    });
+
+  return objects_in_space;
+}
+
+fn load_uom() -> HashMap<String, SpaceObject> {
   let input = fs::read_to_string("./src/day_06_orbit_map/input.txt")
     .expect("Something went wrong reading the input file from the day-folder:");
-  input.lines().map(|orbit| orbit.split(')')).collect()
+
+  return parse_uom(input);
 }
 
 pub fn calculate() {}
@@ -25,7 +66,8 @@ D)I
 E)J
 J)K
 K)L
-";
-    assert_eq!(count_orbits(input), 42);
+"
+    .to_string();
+    assert_eq!(count_orbits(parse_uom(input)), 42);
   }
 }
