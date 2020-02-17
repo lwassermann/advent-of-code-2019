6c75fc872db9676ae59171a18f7c3a6f7b76d1fc
Lars Wassermann <lars@bitcrowd.net>
2020-02-17 10:25:11 +0100

    WIP: Prepare structure for initialization

--- src/day_06_orbit_map/mod.rs
+++ src/day_06_orbit_map/mod.rs
@@ -1,13 +1,23 @@
 use std::collections::HashMap;
 use std::fs;
 
-struct SpaceObject {
+type SolarSystem = HashMap<String, SpaceObject>;
+
+struct SpaceObject<'a> {
   name: String,
-  orbiting: Option<&SpaceObject>,
+  orbiting: Option<&'a SpaceObject<'a>>,
   distance: u32,
 }
 
-fn parse_uom(input: String) -> HashMap<String, SpaceObject> {
+fn parse_orbit(line: &str) -> (&str, &str) {
+  let parts = line.split(')');
+  let a = parts.next().unwrap();
+  let b = parts.next().unwrap();
+  assert!(parts.next().is_none());
+  return (a, b);
+}
+
+fn parse_uom<'a>(input: String) -> SolarSystem<'a> {
   let mut objects_in_space = HashMap::new();
   objects_in_space.insert(
     "COM".to_string(),
@@ -20,17 +30,17 @@ fn parse_uom(input: String) -> HashMap<String, SpaceObject> {
 
   input
     .lines()
-    .map(|orbit| orbit.split(')').collect())
-    .for_each(|[center_identifier, orbiting]| {
-      let center = objects_in_space.get(center_identifier);
+    .map(|orbit| parse_orbit(orbit))
+    .for_each(|(center_identifier, orbiting)| {
+      let center = objects_in_space.get(&center_identifier.to_string());
       let distance = match center {
         Some(object) => object.distance + 1,
         None => 0,
       };
       objects_in_space.insert(
-        orbiting,
+        orbiting.to_string(),
         SpaceObject {
-          name: orbiting,
+          name: orbiting.to_string(),
           orbiting: center,
           distance: distance,
         },
@@ -40,14 +50,21 @@ fn parse_uom(input: String) -> HashMap<String, SpaceObject> {
   return objects_in_space;
 }
 
-fn load_uom() -> HashMap<String, SpaceObject> {
+fn load_uom<'a>() -> SolarSystem<'a> {
   let input = fs::read_to_string("./src/day_06_orbit_map/input.txt")
     .expect("Something went wrong reading the input file from the day-folder:");
 
   return parse_uom(input);
 }
 
-pub fn calculate() {}
+fn count_orbits(universe: &SolarSystem) -> u32 {
+  return 1;
+}
+
+pub fn calculate() {
+  let universe = load_uom();
+  println!("Orbits: {}", count_orbits(&universe));
+}
 
 #[cfg(test)]
 mod tests {
@@ -68,6 +85,6 @@ J)K
 K)L
 "
     .to_string();
-    assert_eq!(count_orbits(parse_uom(input)), 42);
+    assert_eq!(count_orbits(&parse_uom(input)), 42);
   }
 }
